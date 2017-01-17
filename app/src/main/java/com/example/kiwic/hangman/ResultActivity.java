b1e50b1f0628f0854111364f2fd924d0faf845a4
Casper <kiwicasper@hotmail.com>
2017-01-17 09:38:58 +0100

    Working app

--- app/src/main/java/com/example/kiwic/hangman/ResultActivity.java
+++ app/src/main/java/com/example/kiwic/hangman/ResultActivity.java
@@ -1,6 +1,8 @@
 package com.example.kiwic.hangman;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,10 +10,17 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 
 public class ResultActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button restart;
+
+    TextView previusLost;
+    TextView previusWon;
+
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,19 +30,39 @@ public class ResultActivity extends AppCompatActivity implements View.OnClickLis
         ImageView image = (ImageView) findViewById(R.id.img_result);
         TextView text = (TextView) findViewById(R.id.correctWord);
 
+        previusLost = (TextView) findViewById(R.id.previusLost);
+        previusWon = (TextView) findViewById(R.id.previusWon);
+
+
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
         Intent i = getIntent();
         if (i.getExtras().getBoolean("result")){
             text.setText("Du gættede ordet: " + i.getExtras().getString("ord"));
             image.setImageResource(R.mipmap.won);
+            sharedPreferences.edit().putInt("won", (sharedPreferences.getInt("won", 0)+1)).commit();
+            update();
+
         }
         else {
             text.setText("Du kunne ikke gætte ordet: " + i.getExtras().getString("ord"));
             image.setImageResource(R.mipmap.lost);
+            sharedPreferences.edit().putInt("lost", (sharedPreferences.getInt("lost", 0)+1)).commit();
+            update();
         }
+
+
+
 
         restart = (Button) findViewById(R.id.btn_restart);
         restart.setOnClickListener(this);
 
+    }
+
+    private void update() {
+
+        previusLost.setText("Du har i alt vundet " + sharedPreferences.getInt("won", 0) + " gange");
+        previusWon.setText("Du har i alt tabt " + sharedPreferences.getInt("lost", 0) + " gange");
     }
 
     @Override
